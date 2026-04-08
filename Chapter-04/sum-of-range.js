6ce4356ff3b5090bd243123ea79839cffedf102d
Ammar Immad <126868193+Ammar-Technology@users.noreply.github.com>
2026-04-08 05:24:03 +0200

    Enhance Range function with step parameter
    Refactored Range function to include step parameter for custom increments.

--- Chapter-04/sum-of-range.js
+++ Chapter-04/sum-of-range.js
@@ -1,1 +1,33 @@
+function Range(start, end) {
+    const limitedArray = [];
+    for (let i = start; i <= end; i++) {
+        limitedArray.push(i);
+    }
+    return limitedArray;
+}
 
+
+function sum(array) {
+    let arrayCounter = 0;
+    for (let i = 0; i < array.length; i++) {
+        arrayCounter += array[i];
+    }
+    return arrayCounter;
+}
+
+
+function Range(start, end, step = start < end ? 1 : -1) {
+    const limitedArray = [];
+    if (step > 0) {
+        for (let i = start; i <= end; i += step) {
+            limitedArray.push(i);
+        }
+    } else if (step < 0) {
+        for (let i = start; i >= end; i += step) {
+            limitedArray.push(i);
+        }
+    } else {
+        console.log("Step cannot be zero");
+    }
+    return limitedArray;
+}
