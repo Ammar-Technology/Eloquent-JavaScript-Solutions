8576a48e9a3aebbf69917145707d22aa52d465e5
Ammar Immad <126868193+Ammar-Technology@users.noreply.github.com>
2026-04-08 13:44:04 +0200

    Update a-list.js

--- Chapter-04/a-list.js
+++ Chapter-04/a-list.js
@@ -1,1 +1,13 @@
-
+function arrayToList(Array) {
+    let list = null;
+    for (let i = Array.length-1 ; i >= 0; i--) {
+        let object = {
+            value: null,
+            rest: null,
+        }
+        object.value = Array[i];
+        object.rest = list;
+        list = object;
+    }
+    return list;
+}
