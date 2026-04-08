32fc0e2a48a01f3c12ba3c45a0690bfb48ae2646
Ammar Immad <126868193+Ammar-Technology@users.noreply.github.com>
2026-04-08 12:42:20 +0200

    Implement function to reverse an array

--- Chapter-04/reversing-an-array.js
+++ Chapter-04/reversing-an-array.js
@@ -1,1 +1,7 @@
-
+function reverseArray(originalArray) {
+    let newArray = [];
+    for (let i = originalArray.length - 1; i >= 0; i--) {
+        newArray.push(originalArray[i]);
+    }
+    return newArray;
+}
