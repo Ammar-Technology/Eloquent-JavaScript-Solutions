da56e44e65c1284beb1dcb3c4d700b033d776421
Ammar Immad <126868193+Ammar-Technology@users.noreply.github.com>
2026-07-05 13:34:07 +0300

    Implement array flattening with manual and reduce methods
    Added a manual flattening function and a correct solution using reduce.

--- Chapter-05/Flattening.js
+++ Chapter-05/Flattening.js
@@ -1,1 +1,20 @@
+// Solution without meeting the reduce and concat requirements
+let container = [];
+let arrays1 = [[1, 2, 3], [4, 5], [6]];
 
+function flatteningArray (x, y) {
+    for (let i = 0; i < y.length; i++) {
+        for (let j = 0; j < y[i].length; j++) {
+            x.push(y[i][j])
+        }
+    }
+    return x;
+}
+console.log(flatteningArray(container, arrays1));
+
+
+
+// The correct solution that meets the requirements
+let arrays2 = [[1, 2, 3], [4, 5], [6]];
+let ruslt = arrays2.reduce((x,y) => x.concat(y), []);
+console.log(ruslt);
